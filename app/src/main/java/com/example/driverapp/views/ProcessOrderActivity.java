81857cf65f5747d515a199eb11f9dbd93bd775a8
arpangroup <arpan@2020>
2020-09-27 05:26:40 +0530

    First Commit

---
+++ app/src/main/java/com/example/driverapp/views/ProcessOrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.driverapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.driverapp.R;
+
+public class ProcessOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_order);
+    }
+}
---
+++ app/src/main/java/com/example/driverapp/views/order/DeliverOrderFragment.java
@@ -0,0 +1,47 @@
+package com.example.driverapp.views.order;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.driverapp.R;
+import com.example.driverapp.databinding.FragmentDeliverOrderBinding;
+import com.example.driverapp.databinding.FragmentReachPickUpLocationBinding;
+import com.example.driverapp.viewmodels.AuthenticationViewModel;
+
+public class DeliverOrderFragment extends Fragment {
+    private final String TAG = this.getClass().getSimpleName();
+
+    private FragmentDeliverOrderBinding mBinding;
+    AuthenticationViewModel authenticationViewModel;
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mBinding = FragmentDeliverOrderBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View rootView, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(rootView, savedInstanceState);
+
+
+        // Initialize ViewModel
+        authenticationViewModel = new ViewModelProvider(requireActivity()).get(AuthenticationViewModel.class);
+        authenticationViewModel.init();
+
+        // Initialize NavController
+        navController = Navigation.findNavController(rootView);
+
+    }
+}
